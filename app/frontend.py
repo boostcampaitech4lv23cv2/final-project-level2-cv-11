f5918bc89e37121b861345738a60b07cf792c0e2
teajun <ta1231@ajou.ac.kr>
2023-01-11 01:34:50 +0000

    feat: annotation(수정본)

--- app/frontend.py
+++ app/frontend.py
@@ -13,7 +13,7 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
    st.header("Background")
-   background_file = st.file_uploader('Choose an image', type=['jpg', 'jpeg', 'png'], key='background')
+   background_file = st.file_uploader('Choose an image', type=['jpg', 'jpeg', 'png'], key='background',)
    if background_file:
         background_image_bytes = background_file.getvalue()
         background_image = Image.open(io.BytesIO(background_image_bytes))
@@ -25,34 +25,34 @@ with col2:
     if typical_image:
         img = Image.open(typical_image)
         w, h = img.size
-    else:
-        w, h = 100, 100
+        inner_width = 400
+        canvas_width = inner_width
+        canvas_height = h * (canvas_width / w)
 
-    inner_width = 400
-    canvas_width = inner_width
-    canvas_height = h * (canvas_width / w)
 
-    typical_canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-    stroke_width=3,
-    stroke_color="#eee",
-    background_color="#eee",
-    background_image=Image.open(typical_image) if typical_image else None,
-    update_streamlit=True,
-    height=canvas_height,
-    width=canvas_width,
-    drawing_mode='rect',
-    point_display_radius=0,
-    key='typical_canvas'
-    )
+    
 
-    if len(typical_canvas_result.json_data['objects']) > 0:
-        objects = pd.json_normalize(typical_canvas_result.json_data["objects"]) # need to convert obj to str because PyArrow
-        for col in objects.select_dtypes(include=['object']).columns:
-            objects[col] = objects[col].astype("str")
-        st.dataframe(objects[['left', 'top', 'width', 'height']])
-    else:
-        pass
+        typical_canvas_result = st_canvas(
+        fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+        stroke_width=3,
+        stroke_color="#eee",
+        background_color="#eee",
+        background_image=Image.open(typical_image) if typical_image else None,
+        update_streamlit=True,
+        height=canvas_height,
+        width=canvas_width,
+        drawing_mode='rect',
+        point_display_radius=0,
+        key='typical_canvas'
+        )
+
+        if len(typical_canvas_result.json_data['objects']) > 0:
+            objects = pd.json_normalize(typical_canvas_result.json_data["objects"]) # need to convert obj to str because PyArrow
+            for col in objects.select_dtypes(include=['object']).columns:
+                objects[col] = objects[col].astype("str")
+            st.dataframe(objects[['left', 'top', 'width', 'height']])
+        else:
+            pass
 
 
 with col3:
@@ -61,38 +61,37 @@ with col3:
     if untypical_image:
         img = Image.open(untypical_image)
         w, h = img.size
-    else:
-        w, h = 100, 100
 
-    inner_width = 300
-    canvas_width = inner_width
-    canvas_height = h * (canvas_width / w)
+        inner_width = 400
+        canvas_width = inner_width
+        canvas_height = h * (canvas_width / w)
 
-    untypical_canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-    stroke_width=3,
-    stroke_color="#eee",
-    background_color="#eee",
-    background_image=Image.open(untypical_image) if untypical_image else None,
-    update_streamlit=True,
-    height=canvas_height,
-    width=canvas_width,
-    drawing_mode='rect',
-    point_display_radius=0,
-    key='untypical_canvas'
-    )
+        untypical_canvas_result = st_canvas(
+        fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+        stroke_width=3,
+        stroke_color="#eee",
+        background_color="#eee",
+        background_image=Image.open(untypical_image) if untypical_image else None,
+        update_streamlit=True,
+        height=canvas_height,
+        width=canvas_width,
+        drawing_mode='rect',
+        point_display_radius=0,
+        key='untypical_canvas'
+        )
 
-    if len(untypical_canvas_result.json_data['objects']) > 0:
-        objects = pd.json_normalize(untypical_canvas_result.json_data["objects"]) # need to convert obj to str because PyArrow
-        for col in objects.select_dtypes(include=['object']).columns:
-            objects[col] = objects[col].astype("str")
-        st.dataframe(objects[['left', 'top', 'width', 'height']])
-    else:
-        pass
+        if len(untypical_canvas_result.json_data['objects']) > 0:
+            objects = pd.json_normalize(untypical_canvas_result.json_data["objects"]) # need to convert obj to str because PyArrow
+            for col in objects.select_dtypes(include=['object']).columns:
+                objects[col] = objects[col].astype("str")
+            st.dataframe(objects[['left', 'top', 'width', 'height']])
+        else:
+            pass
+        # pass
 
 st.markdown("----", unsafe_allow_html=True)
 
-if background_file and typical_file and untypical_file:
+if background_file and typical_image and untypical_image:
     bcol1, bcol2, bcol3 , bcol4, bcol5= st.columns(5)
     with bcol1:
         pass
