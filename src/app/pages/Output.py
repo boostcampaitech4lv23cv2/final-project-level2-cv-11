bcc3657d4d72e28fe35b4ae96fc91f7f20c91ee6
Yera10 <yera9410@gmail.com>
2023-01-17 05:59:17 +0000

    feat : output page UI

--- src/app/pages/Output.py
+++ src/app/pages/Output.py
@@ -9,11 +9,11 @@ st.title('Toon-ranslate')
 cols = st.columns([1, 5, 1, 5, 1])
 with cols[1]:
     st.markdown("<h3 style='text-align: center;'>원본</h3>", unsafe_allow_html=True)
-    img_original = Image.open('/opt/ml/level3_productserving-level3-cv-11/app/test_file/windbreaker_ko.PNG')
+    img_original = Image.open('/opt/ml/level3_productserving-level3-cv-11/src/app/test_file/windbreaker_ko.PNG')
     st.image(img_original)
 with cols[3]:
     st.markdown("<h3 style='text-align: center;'>결과물</h3>", unsafe_allow_html=True)
-    img_output = Image.open('/opt/ml/level3_productserving-level3-cv-11/app/test_file/windbreaker_en.PNG')
+    img_output = Image.open('/opt/ml/level3_productserving-level3-cv-11/src/app/test_file/windbreaker_en.PNG')
     st.image(img_output)
 
 bcols = st.columns([5, 1, 1, 1, 5])
@@ -22,7 +22,7 @@ with bcols[1]:
 with bcols[2]:
     st.button('Share')
 with bcols[3]:
-    with open('/opt/ml/level3_productserving-level3-cv-11/app/test_file/windbreaker_en.PNG', 'rb') as img_file:
+    with open('/opt/ml/level3_productserving-level3-cv-11/src/app/test_file/windbreaker_en.PNG', 'rb') as img_file:
         st.download_button(
             'Download', 
             img_file, 
