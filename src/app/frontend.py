6878ee6aba7a098e183b725e9aff7ebc97599656
Yera10 <yera9410@gmail.com>
2023-01-17 04:56:22 +0000

    fix : papago 메소드 수정 & OCR 모델 호출 수정
    - Typical_Pipeline의 papago 메소드를 bbox를 번역하는 것에서 text를 번역하는 것으로 변경
    
    - OCR 모델을 ocr_flag가 True일 때만 실행되도록 변경
    
    - 텍스트 번역을 Typical_Pipeline을 사용하도록 변경

--- src/app/frontend.py
+++ src/app/frontend.py
@@ -2,20 +2,6 @@ import streamlit as st
 from st_component import areas as st_area
 from st_component import buttons as st_buttons
 
-import sys
-from os import path
-sys.path.append(path.dirname( path.dirname( path.abspath(__file__) ) ))
-import model
-from pipeline import typical_pipeline
-
-# 각 모델 사용할 때
-# model.Clova_OCR
-# model.Papago_MT
-# model.Tesseract_OCR
-# model.Typical_FC 로 사용하면 됨
-
-# 이미지가 업로드 될 때마다 호출되는 콜백함수
-# '(un)typical_ocr_flag' 를 True로 설정함
 def set_ocr_flag(key):
     """
     이미지가 업로드 될 때마다 호출되는 콜백함수
@@ -44,16 +30,9 @@ with col2:
                                      key='typical_uploader',
                                      on_change=set_ocr_flag,
                                      args=('typical_ocr_flag',))
-    # TODO: 나중에 ocr_results는 함수 바깥에서 받아와야함, ex anno_area(input_img, key, ocr_results)
-    if typical_image:
-        Typical_pipeline = typical_pipeline.Typical_Pipeline()
-        clova_result = Typical_pipeline.clova_ocr(typical_image.getvalue())
-        st_area.anno_area(typical_image, 'typical', clova_result)
-        mt_result = Typical_pipeline.papago(clova_result)
-        print(mt_result)
     
-    # clova_result = [[[555, 417], [958, 545], '서른다섯 배, 오백만 주로 계약했다.']]
-    # mt_result = [[[555, 417], [958, 545], '서른다섯 배, 오백만 주로 계약했다.', 'Thirty-five times, five million shares signed.']]
+    if typical_image:
+        st_area.anno_area(typical_image, 'typical')
 
 with col3:
     st.header("UnTypical Text")
