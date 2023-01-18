55f42b2030c9995812717636fceeed79eb308261
KH <ganghe74@gmail.com>
2023-01-18 18:37:35 +0900

    feat: `black`, `pre-commit` 패키지 추가
    poetry dev 환경에 두 패키지를 추가하고 프론트엔드 코드에 `black` 을 적용시켰음
    
    Issue: #27

--- src/app/st_component/areas.py
+++ src/app/st_component/areas.py
@@ -7,14 +7,16 @@ from typing import Literal
 
 import sys
 from os import path
-sys.path.append(path.dirname( path.dirname( path.dirname( path.abspath(__file__) ) ) ))
+
+sys.path.append(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
 from pipeline import typical_pipeline
 
 # initial_drawing object 템플릿
+# fmt: off
 RECT = {'type': 'rect', 'version': '4.4.0', 'originX': 'left', 'originY': 'top', 'left': 0, 'top': 0, 'width': 0, 'height': 0, 'fill': 'rgba(255, 165, 0, 0.3)', 'stroke': '#eee', 'strokeWidth': 3, 'strokeDashArray': None, 'strokeLineCap': 'butt', 'strokeDashOffset': 0, 'strokeLineJoin': 'miter', 'strokeUniform': True, 'strokeMiterLimit': 4, 'scaleX': 1, 'scaleY': 1, 'angle': 0, 'flipX': False, 'flipY': False, 'opacity': 1, 'shadow': None, 'visible': True, 'backgroundColor': '', 'fillRule': 'nonzero', 'paintFirst': 'fill', 'globalCompositeOperation': 'source-over', 'skewX': 0, 'skewY': 0, 'rx': 0, 'ry': 0}
 
 
-def anno_area(input_img, key: Literal['typical', 'untypical']):
+def anno_area(input_img, key: Literal["typical", "untypical"]):
     # 이미지 사이즈 변경
     img = Image.open(input_img)
     w, h = img.size
@@ -23,32 +25,32 @@ def anno_area(input_img, key: Literal['typical', 'untypical']):
 
     # 파이프라인 모델
     Typical_pipeline = typical_pipeline.Typical_Pipeline()
-    
+
     # OCR 한번만 실행
-    if st.session_state[f'{key}_ocr_flag']:
+    if st.session_state[f"{key}_ocr_flag"]:
         # OCR inference
         uploaded_file = input_img
         image_bytes = input_img.getvalue()
-        files = [('files', (uploaded_file.name, image_bytes, uploaded_file.type))]
-        ocr_results = requests.post('http://localhost:30002/ocr', files=files).json()
+        files = [("files", (uploaded_file.name, image_bytes, uploaded_file.type))]
+        ocr_results = requests.post("http://localhost:30002/ocr", files=files).json()
 
         # initial_drawing 포맷으로 변환
         rects = []
         for i, result in enumerate(ocr_results):
             rect = RECT.copy()
-            rect['left'] = result[0][0] / w * canvas_width
-            rect['top'] = result[0][1] / h * canvas_height
-            rect['width'] = (result[1][0] - result[0][0]) / w * canvas_width
-            rect['height'] = (result[1][1] - result[0][1]) / h * canvas_height
+            rect["left"] = result[0][0] / w * canvas_width
+            rect["top"] = result[0][1] / h * canvas_height
+            rect["width"] = (result[1][0] - result[0][0]) / w * canvas_width
+            rect["height"] = (result[1][1] - result[0][1]) / h * canvas_height
             rects.append(rect)
-            st.session_state[f'{key}_anno{i}'] = result[2] # OCR 텍스트를 session_state에 저장
+            st.session_state[f"{key}_anno{i}"] = result[2]  # OCR 텍스트를 session_state에 저장
 
-        initial_drawing = {'objects': rects}
-        st.session_state[f'{key}_initial_drawing'] = initial_drawing
-        st.session_state[f'{key}_ocr_flag'] = False
+        initial_drawing = {"objects": rects}
+        st.session_state[f"{key}_initial_drawing"] = initial_drawing
+        st.session_state[f"{key}_ocr_flag"] = False
 
-    flag = st.checkbox('편집', key=f'{key}_edit')
-    drawing_mode = 'transform' if flag else 'rect'
+    flag = st.checkbox("편집", key=f"{key}_edit")
+    drawing_mode = "transform" if flag else "rect"
 
     # 캔버스
     canvas_result = st_canvas(
@@ -62,26 +64,29 @@ def anno_area(input_img, key: Literal['typical', 'untypical']):
         width=canvas_width,
         drawing_mode=drawing_mode,
         point_display_radius=0,
-        key=f'{key}_canvas',
-        initial_drawing=st.session_state[f'{key}_initial_drawing'],
+        key=f"{key}_canvas",
+        initial_drawing=st.session_state[f"{key}_initial_drawing"],
     )
 
     # annotation 값
     if canvas_result.json_data is not None:
-        new_objects = canvas_result.json_data['objects']
+        new_objects = canvas_result.json_data["objects"]
         for i, obj in enumerate(new_objects):
-            with st.expander(f'anntation {i}'):
-                obj['text'] = st.text_input('인식된 글자', key=f'{key}_anno{i}')
-                st.text(f"left:{int(obj['left']/canvas_width*w)}  top:{int(obj['top']*h/canvas_height)}  width:{int(obj['width']/canvas_width*w)}  height:{int(obj['height']*h/canvas_height)}  text:{obj['text']}")
-                params = {'text': f"{obj['text']}"}
+            with st.expander(f"anntation {i}"):
+                obj["text"] = st.text_input("인식된 글자", key=f"{key}_anno{i}")
+                st.text(
+                    f"left:{int(obj['left']/canvas_width*w)}  top:{int(obj['top']*h/canvas_height)}  width:{int(obj['width']/canvas_width*w)}  height:{int(obj['height']*h/canvas_height)}  text:{obj['text']}"
+                )
+                params = {"text": f"{obj['text']}"}
                 r = requests.post(f"http://localhost:30002/mt", params=params)
-                translation = r.json() if r.status_code == 200 else ''
-                st.text_input('번역된 글자', translation, key=f'{key}_translated{i}')
+                translation = r.json() if r.status_code == 200 else ""
+                st.text_input("번역된 글자", translation, key=f"{key}_translated{i}")
     else:
         pass
+
 
 # TODO: 삭제해도 되는지 확인 필요
 def imag_show(img_file):
     background_image_bytes = img_file.getvalue()
     background_image = Image.open(io.BytesIO(background_image_bytes))
-    st.image(background_image, caption='Uploaded Image')
+    st.image(background_image, caption="Uploaded Image")
--- src/app/st_component/buttons.py
+++ src/app/st_component/buttons.py
@@ -1,8 +1,9 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 
+
 def btn_generation():
-    bcol1, bcol2, bcol3 , bcol4, bcol5= st.columns(5)
+    bcol1, bcol2, bcol3, bcol4, bcol5 = st.columns(5)
     with bcol1:
         pass
     with bcol2:
@@ -12,5 +13,5 @@ def btn_generation():
     with bcol4:
         pass
     with bcol5:
-        if st.button('generatoon', key='generation_button'):
-            switch_page('result')
+        if st.button("generatoon", key="generation_button"):
+            switch_page("result")
