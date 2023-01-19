f7633a693b393d47c96186bf70e6588d11fd03b3
Yera10 <yera9410@gmail.com>
2023-01-19 02:10:20 +0000

    fix : font_path 상대경로로 수정

--- src/app/st_component/buttons.py
+++ src/app/st_component/buttons.py
@@ -8,11 +8,18 @@ import os
 
 
 ## 폰트 파일 위치
-font_path = '/opt/ml/Final_Project/level3_productserving-level3-cv-11/data/fonts/typical'
+font_path = os.path.join(
+    os.path.dirname(
+        os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    ),
+    "data",
+    "fonts",
+    "typical",
+)
 
 
 def btn_generation(background_file, translated_list, font_list):
-    bcol1, bcol2, bcol3 , bcol4, bcol5= st.columns(5)
+    bcol1, bcol2, bcol3, bcol4, bcol5 = st.columns(5)
     with bcol1:
         pass
     with bcol2:
@@ -22,26 +29,27 @@ def btn_generation(background_file, translated_list, font_list):
     with bcol4:
         pass
     with bcol5:
-        on_press = st.button('generatoon', key='generation_button')
-            
-    if on_press:        
-        '''
+        on_press = st.button("generatoon", key="generation_button")
+
+    if on_press:
+        """
         background_file = st.file_uploader('Choose an image',
                                       type=['jpg', 'jpeg', 'png'],
                                       key='background',)
-        '''
+        """
         # fig = plt.figure(figsize=(10, 10))
         background_image_bytes = background_file.getvalue()
         back_img = Image.open(BytesIO(background_image_bytes))
         draw = ImageDraw.Draw(back_img)
         for i, translated in enumerate(translated_list):
             font_type = ImageFont.truetype(os.path.join(font_path, font_list[i]), 20)
-            print('font type:', font_type)
-            draw.text((translated[0], translated[1]), translated[2], 'black',
-            font=font_type,
+            print("font type:", font_type)
+            draw.text(
+                (translated[0], translated[1]),
+                translated[2],
+                "black",
+                font=font_type,
             )
-        print('draw: ', draw)
-        print('type: ',type(back_img))
+        print("draw: ", draw)
+        print("type: ", type(back_img))
         st.image(back_img)
-
-        
