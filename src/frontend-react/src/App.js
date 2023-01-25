78fd1272ec4ff940922e6cfcea5ec566509f467d
KH <ganghe74@gmail.com>
2023-01-25 08:20:11 +0000

    style: add prettier hook

--- src/frontend-react/src/App.js
+++ src/frontend-react/src/App.js
@@ -1,126 +1,128 @@
-import { useEffect, useState } from 'react';
-import { UploadOutlined } from '@ant-design/icons';
-import { Button, message, Upload, Col, Divider, Row } from 'antd';
-import { Typography } from 'antd';
-import { fabric } from 'fabric';
-import Editor from './Editor';
+import { useEffect, useState } from "react";
+import { UploadOutlined } from "@ant-design/icons";
+import { Button, message, Upload, Col, Divider, Row } from "antd";
+import { Typography } from "antd";
+import { fabric } from "fabric";
+import Editor from "./Editor";
 const { Title } = Typography;
 
 const props = {
-  name: 'file',
-  accept: 'image/*',
+  name: "file",
+  accept: "image/*",
   maxCount: 1,
   onChange(info) {
-    if (info.file.status !== 'uploading') {
+    if (info.file.status !== "uploading") {
       console.log(info.file, info.fileList);
     }
-    if (info.file.status === 'done') {
+    if (info.file.status === "done") {
       message.success(`${info.file.name} file uploaded successfully`);
-    } else if (info.file.status === 'error') {
+    } else if (info.file.status === "error") {
       message.error(`${info.file.name} file upload failed.`);
-      console.log('error', info)
+      console.log("error", info);
     }
   },
 };
 
 const App = () => {
-  const [back, setBack] = useState(null)
-  const [typical, setTypical] = useState(null)
-  const [typicalFile, setTypicalFile] = useState(null)
+  const [back, setBack] = useState(null);
+  const [typical, setTypical] = useState(null);
+  const [typicalFile, setTypicalFile] = useState(null);
 
-  const makeCustomRequest = (set, setFile=null) => {
+  const makeCustomRequest = (set, setFile = null) => {
     return ({ file, onSuccess }) => {
-      if (setFile) setFile(file)
-      const reader = new FileReader()
+      if (setFile) setFile(file);
+      const reader = new FileReader();
       reader.onload = (event) => {
-        const imgObj = new Image()
-        imgObj.src = event.target.result
+        const imgObj = new Image();
+        imgObj.src = event.target.result;
         imgObj.onload = () => {
-          const image = new fabric.Image(imgObj)
-          set(image)
-        }
-      }
-      reader.readAsDataURL(file)
-      onSuccess()
-    }
-  }
+          const image = new fabric.Image(imgObj);
+          set(image);
+        };
+      };
+      reader.readAsDataURL(file);
+      onSuccess();
+    };
+  };
 
-  const fetchAndSet = (path, set, setFile=null) => {
+  const fetchAndSet = (path, set, setFile = null) => {
     fetch(path)
-      .then(res => res.blob())
-      .then(blob => {
-        if (setFile) setFile(blob)
-        const reader = new FileReader()
+      .then((res) => res.blob())
+      .then((blob) => {
+        if (setFile) setFile(blob);
+        const reader = new FileReader();
         reader.onload = (event) => {
-          const imgObj = new Image()
-          imgObj.src = event.target.result
+          const imgObj = new Image();
+          imgObj.src = event.target.result;
           imgObj.onload = () => {
-            const image = new fabric.Image(imgObj)
-            set(image)
-          }
-        }
-        reader.readAsDataURL(blob)
-      })
-  }
+            const image = new fabric.Image(imgObj);
+            set(image);
+          };
+        };
+        reader.readAsDataURL(blob);
+      });
+  };
 
   useEffect(() => {
-    fetchAndSet('./background.png', setBack)
-    fetchAndSet('./typical.png', setTypical, setTypicalFile)
-  }, [])
+    fetchAndSet("./background.png", setBack);
+    fetchAndSet("./typical.png", setTypical, setTypicalFile);
+  }, []);
 
-  return <div style={{
-      margin: '0 auto',
-      textAlign: 'center',
-    }}>
-    <Title>
-      웹툰 번역
-    </Title>
+  return (
+    <div
+      style={{
+        margin: "0 auto",
+        textAlign: "center",
+      }}
+    >
+      <Title>웹툰 번역</Title>
 
-    <Divider>업로드</Divider>
-    <Row justify="center">
-      <Col span={4}>
-        <Upload
-          customRequest={makeCustomRequest(setBack)}
-          defaultFileList={[
-            {
-              uid: '1',
-              name: 'background.png',
-              url: './background.png'
-            }
-          ]}
-          {...props}
-        >
-          <Button icon={<UploadOutlined />}>배경 업로드</Button>
-        </Upload>
-      </Col>
-      <Col span={4}>
-        <Upload
-          customRequest={makeCustomRequest(setTypical, setTypicalFile)}
-          defaultFileList={[
-            {
-              uid: '1',
-              name: 'typical.png',
-              url: './typical.png'
-            }
-          ]}
-          {...props}
-        >
-          <Button icon={<UploadOutlined />}>대사 업로드</Button>
-        </Upload>
-      </Col>
-      <Col span={4}>
-        <Upload {...props}>
-          <Button icon={<UploadOutlined />}>효과음 업로드(미구현)</Button>
-        </Upload>
-      </Col>
-    </Row>
+      <Divider>업로드</Divider>
+      <Row justify="center">
+        <Col span={4}>
+          <Upload
+            customRequest={makeCustomRequest(setBack)}
+            defaultFileList={[
+              {
+                uid: "1",
+                name: "background.png",
+                url: "./background.png",
+              },
+            ]}
+            {...props}
+          >
+            <Button icon={<UploadOutlined />}>배경 업로드</Button>
+          </Upload>
+        </Col>
+        <Col span={4}>
+          <Upload
+            customRequest={makeCustomRequest(setTypical, setTypicalFile)}
+            defaultFileList={[
+              {
+                uid: "1",
+                name: "typical.png",
+                url: "./typical.png",
+              },
+            ]}
+            {...props}
+          >
+            <Button icon={<UploadOutlined />}>대사 업로드</Button>
+          </Upload>
+        </Col>
+        <Col span={4}>
+          <Upload {...props}>
+            <Button icon={<UploadOutlined />}>효과음 업로드(미구현)</Button>
+          </Upload>
+        </Col>
+      </Row>
 
-    <Divider>편집</Divider>
+      <Divider>편집</Divider>
 
-    <div style={{ border: '1px solid' }}>
-      <Editor background={back} typical={typical} typicalFile={typicalFile} />
+      <div style={{ border: "1px solid" }}>
+        <Editor background={back} typical={typical} typicalFile={typicalFile} />
+      </div>
     </div>
-  </div>
+  );
 };
 
 export default App;
--- src/frontend-react/src/Editor.js
+++ src/frontend-react/src/Editor.js
@@ -1,9 +1,9 @@
 import React, { useRef, useEffect, useState } from "react";
 import { message, Divider, Checkbox, Button } from "antd";
-import { PlusOutlined } from '@ant-design/icons';
+import { PlusOutlined } from "@ant-design/icons";
 import { fabric } from "fabric";
-import Box from './components/Box';
-import './myFabric.css';
+import Box from "./components/Box";
+import "./myFabric.css";
 
 const MyFabric = ({ background, typical, typicalFile }) => {
   const canvasRef = useRef(null);
@@ -12,12 +12,14 @@ const MyFabric = ({ background, typical, typicalFile }) => {
   useEffect(() => {
     const initFabric = () => {
       fabricRef.current = new fabric.Canvas(canvasRef.current);
-      const f = e => {e.target.refresh(e.target)}
+      const f = (e) => {
+        e.target.refresh(e.target);
+      };
       fabricRef.current.on({
-        'object:modified': f,
-        'object:moving': f,
-        'object:scaling': f,
-      })
+        "object:modified": f,
+        "object:moving": f,
+        "object:scaling": f,
+      });
     };
     const disposeFabric = () => {
       fabricRef.current.dispose();
@@ -30,179 +32,200 @@ const MyFabric = ({ background, typical, typicalFile }) => {
     };
   }, []);
 
-  const [prevTypical, setPrevTypical] = useState(null)
-  const [backChecked, setBackChecked] = useState(true)
-  const [typicalChecked, setTypicalChecked] = useState(true)
+  const [prevTypical, setPrevTypical] = useState(null);
+  const [backChecked, setBackChecked] = useState(true);
+  const [typicalChecked, setTypicalChecked] = useState(true);
   const onBackChanged = (e) => {
-    setBackChecked(e.target.checked)
-  }
+    setBackChecked(e.target.checked);
+  };
   useEffect(() => {
-    if (prevTypical !== null) fabricRef.current?.remove(prevTypical)
-    setPrevTypical(typical)
-  }, [typical])
+    if (prevTypical !== null) fabricRef.current?.remove(prevTypical);
+    setPrevTypical(typical);
+  }, [typical]);
   useEffect(() => {
-    const back = backChecked ? background : null
+    const back = backChecked ? background : null;
     fabricRef.current?.setBackgroundImage(back, () => {
-      fabricRef.current?.renderAll()
-    })
-  }, [backChecked, background])
+      fabricRef.current?.renderAll();
+    });
+  }, [backChecked, background]);
 
   const onTypicalChanged = (e) => {
-    setTypicalChecked(e.target.checked)
-  }
+    setTypicalChecked(e.target.checked);
+  };
   useEffect(() => {
     if (typical !== null) {
-      typical.selectable = false
-      typical.evented = false
-      fabricRef.current?.add(typical)
+      typical.selectable = false;
+      typical.evented = false;
+      fabricRef.current?.add(typical);
     }
-  }, [typical])
+  }, [typical]);
   useEffect(() => {
     if (typical !== null) {
-      typical.visible = typicalChecked
-      fabricRef.current?.renderAll()
+      typical.visible = typicalChecked;
+      fabricRef.current?.renderAll();
     }
-  }, [typicalChecked])
+  }, [typicalChecked]);
 
-
-  const [OCRLoading, setOCRLoading] = useState(false)
+  const [OCRLoading, setOCRLoading] = useState(false);
   const reqOCR = () => {
-    setOCRLoading(true)
+    setOCRLoading(true);
 
-    const formData = new FormData()
-    formData.append('file', typicalFile)
+    const formData = new FormData();
+    formData.append("file", typicalFile);
 
-    fetch('http://49.50.160.104:30002/txt_extraction/', {
-      method: 'POST',
-      body: formData
+    fetch("http://49.50.160.104:30002/txt_extraction/", {
+      method: "POST",
+      body: formData,
     })
       .then((response) => {
-        return response.json()
+        return response.json();
       })
-      .then(data => {
-        const { ocr_result, font_result } = data
+      .then((data) => {
+        const { ocr_result, font_result } = data;
         for (let i = 0; i < ocr_result.length; i++) {
-          const [p1, p2, text] = ocr_result[i]
-          const [x1, y1] = p1
-          const [x2, y2] = p2
-          addBox(x1, y1, x2 - x1, y2 - y1, text, font_result[i])
+          const [p1, p2, text] = ocr_result[i];
+          const [x1, y1] = p1;
+          const [x2, y2] = p2;
+          addBox(x1, y1, x2 - x1, y2 - y1, text, font_result[i]);
         }
-        message.success('OCR에 성공했습니다.')
+        message.success("OCR에 성공했습니다.");
       })
-      .catch(error => {
-        console.error(error)
-        message.error('OCR에 실패했습니다.')
+      .catch((error) => {
+        console.error(error);
+        message.error("OCR에 실패했습니다.");
       })
       .finally(() => {
-        setOCRLoading(false)
-      })
-  }
+        setOCRLoading(false);
+      });
+  };
 
-  const [boxes, setBoxes] = useState([])
-  const idRef = useRef(0)
-  const addBox = (left, top, width, height, text, font='NanumGothic') => {
+  const [boxes, setBoxes] = useState([]);
+  const idRef = useRef(0);
+  const addBox = (left, top, width, height, text, font = "NanumGothic") => {
     const box = new fabric.Rect({
-      top, left, width, height,
-      fill: 'rgba(0, 0, 0, 0.1)',
-      stroke: 'red',
+      top,
+      left,
+      width,
+      height,
+      fill: "rgba(0, 0, 0, 0.1)",
+      stroke: "red",
       strokeWidth: 1,
+    });
+    box.textKor = text;
+    box.textEng = "";
+    box.fontFamily = font;
+    box.fontSize = 40;
+    box.id = idRef.current++;
 
-    })
-    box.textKor = text
-    box.textEng = ''
-    box.fontFamily = font
-    box.fontSize = 40
-    box.id = idRef.current++
+    fabricRef.current.add(box);
 
-    fabricRef.current.add(box)
-
-    setBoxes([...boxes, box])
-  }
+    setBoxes([...boxes, box]);
+  };
   const delBox = (box) => {
-    setBoxes(boxes.filter(b => b.id !== box.id))
-    fabricRef.current.remove(box)
-  }
+    setBoxes(boxes.filter((b) => b.id !== box.id));
+    fabricRef.current.remove(box);
+  };
 
   useEffect(() => {
-    console.log('boxes', boxes)
-  }, [boxes])
+    console.log("boxes", boxes);
+  }, [boxes]);
 
   const convertBox = (box) => {
-    console.log('onConvertBox', box)
-    const { textKor, textEng, font, top, left, width, height } = box
+    console.log("onConvertBox", box);
+    const { textKor, textEng, font, top, left, width, height } = box;
     const textbox = new fabric.Textbox(textEng, {
-      top, left, width, height,
-    })
-    textbox.textKor = textKor
-    textbox.textEng = textEng
-    textbox.font = font
-    textbox.id = box.id
+      top,
+      left,
+      width,
+      height,
+    });
+    textbox.textKor = textKor;
+    textbox.textEng = textEng;
+    textbox.font = font;
+    textbox.id = box.id;
     textbox.on({
-      'object:modified': (e) => {
-        e.target.refresh()
-      }
-    })
-    fabricRef.current.add(textbox)
-    fabricRef.current.remove(box)
-    setBoxes(boxes.map(b => b.id === box.id ? textbox : b))
-  }
+      "object:modified": (e) => {
+        e.target.refresh();
+      },
+    });
+    fabricRef.current.add(textbox);
+    fabricRef.current.remove(box);
+    setBoxes(boxes.map((b) => (b.id === box.id ? textbox : b)));
+  };
 
-  return <div>
-    <div
-      style={{
-        padding: 10
-      }}
-    >
-      <Checkbox onChange={onBackChanged} checked={backChecked}>배경 보이기</Checkbox>
-      <Checkbox onChange={onTypicalChanged} checked={typicalChecked}>대사 보이기</Checkbox>
-      <Button onClick={reqOCR} loading={OCRLoading}>OCR 수행</Button>
-      <Button>대사 일괄 변환(미구현)</Button>
-      <Button
-        onClick={
-          () => {
-            const data = fabricRef.current.toDataURL({ format: 'png' })
-            const a = document.createElement('a')
-            a.href = data
-            a.download = 'result.png'
-            a.click()
-          }
-        }
-        > 다운로드
-      </Button>
-    </div>
-
-    <div className="red">
-      <div className="item">
-        <canvas
-          ref={canvasRef}
-          width={800}
-          height={800}
-          style={{
-            border: "1px solid black",
-          }}
-        />
-      </div>
-      <div className="item">
-        <Divider>대사 목록</Divider>
-        {
-          boxes.length > 0 &&
-          boxes.map((box, i) =>
-            <Box key={box.id} i={i} rect={box} delBox={() => {delBox(box)}}
-              convertBox={() => {convertBox(box)}}
-              renderAll={() => {fabricRef.current?.renderAll()}}
-            />
-          )
-          || <div style={{margin: 50}}>대사가 없습니다</div>
-        }
+  return (
+    <div>
+      <div
+        style={{
+          padding: 10,
+        }}
+      >
+        <Checkbox onChange={onBackChanged} checked={backChecked}>
+          배경 보이기
+        </Checkbox>
+        <Checkbox onChange={onTypicalChanged} checked={typicalChecked}>
+          대사 보이기
+        </Checkbox>
+        <Button onClick={reqOCR} loading={OCRLoading}>
+          OCR 수행
+        </Button>
+        <Button>대사 일괄 변환(미구현)</Button>
         <Button
           onClick={() => {
-            addBox(0, 0, 100, 100, '')
+            const data = fabricRef.current.toDataURL({ format: "png" });
+            const a = document.createElement("a");
+            a.href = data;
+            a.download = "result.png";
+            a.click();
           }}
-          icon={<PlusOutlined />}
-        >대사 추가하기</Button>
+        >
+          {" "}
+          다운로드
+        </Button>
+      </div>
+
+      <div className="red">
+        <div className="item">
+          <canvas
+            ref={canvasRef}
+            width={800}
+            height={800}
+            style={{
+              border: "1px solid black",
+            }}
+          />
+        </div>
+        <div className="item">
+          <Divider>대사 목록</Divider>
+          {(boxes.length > 0 &&
+            boxes.map((box, i) => (
+              <Box
+                key={box.id}
+                i={i}
+                rect={box}
+                delBox={() => {
+                  delBox(box);
+                }}
+                convertBox={() => {
+                  convertBox(box);
+                }}
+                renderAll={() => {
+                  fabricRef.current?.renderAll();
+                }}
+              />
+            ))) || <div style={{ margin: 50 }}>대사가 없습니다</div>}
+          <Button
+            onClick={() => {
+              addBox(0, 0, 100, 100, "");
+            }}
+            icon={<PlusOutlined />}
+          >
+            대사 추가하기
+          </Button>
+        </div>
       </div>
     </div>
-  </div>
+  );
 };
 
 export default MyFabric;
