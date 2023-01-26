324dac2d29766da67ac28acf1a670e6b08ca1d68
KH <ganghe74@gmail.com>
2023-01-26 21:14:35 +0900

    fix: 폰트 분류 모델이 CPU에서 동작 안하는 에러 수정
    모델 불러오는 방식을 수정함
    
    torch 1.7.0 에서 resnet50 생성자에 weights 인자가 없어서 에러가 발생하는 부분 수정

--- src/model/font_classifier/model_font.py
+++ src/model/font_classifier/model_font.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-
 ## efficientnet_b0
 # from torchvision.models import efficientnet_b0
 # class EfficientNet_B0(nn.Module):
@@ -26,15 +25,16 @@ import torch.nn.functional as F
 #     def forward(self, x):
 #         output = self.backbone(x)
 #         return output
-    
-    
-    
+
+
 ## resnet50
 from torchvision.models import resnet50
+
+
 class ResNet50(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
-        self.backbone = resnet50(weights=None)
+        self.backbone = resnet50()
         self.backbone.fc = nn.Sequential(
             nn.Linear(2048, 1024),
             nn.LeakyReLU(),
@@ -45,9 +45,9 @@ class ResNet50(nn.Module):
             nn.Linear(512, 128),
             nn.LeakyReLU(),
             nn.BatchNorm1d(128),
-            nn.Linear(128, num_classes)
+            nn.Linear(128, num_classes),
         )
-        
+
     def forward(self, x):
         x = self.backbone(x)
         return x
--- src/model/font_classifier/typical_FC.py
+++ src/model/font_classifier/typical_FC.py
@@ -3,33 +3,50 @@ import os
 from importlib import import_module
 import torch
 import sys
-sys.path.append(os.path.dirname(__file__))
 from collections import defaultdict
-from dataset_font import InfDataset
 
+sys.path.append(os.path.dirname(__file__))
+from dataset_font import InfDataset
+from model_font import ResNet50
 
 
 # 경고 off
 import warnings
-warnings.filterwarnings(action='ignore')
+
+warnings.filterwarnings(action="ignore")
+
 
 class Typical_FC:
-    
-    
     def __init__(self, type):
         self.use_cuda = torch.cuda.is_available()
         self.device = torch.device("cuda" if self.use_cuda else "cpu")
-        self.resize = (256,256)
+        self.resize = (256, 256)
         self.batch_size = 2
-        self.font_data_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))), "data/fonts/typical")
+        self.font_data_dir = os.path.join(
+            os.path.dirname(
+                os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+            ),
+            "data/fonts/typical",
+        )
         self.font_list = os.listdir(self.font_data_dir)
-        self.model = torch.load(os.path.join(os.path.dirname(__file__),"weights",type,"weight.pth"), map_location = self.device)
-        
+
+        # TODO: 수정할 요소가 많은 코드임
+        # 현재 팀에서 사용중인 가중치 (pkl) 타입은 아래와 같음
+        # <class 'torch.nn.parallel.data_parallel.DataParallel'>
+        pkl = torch.load(
+            os.path.join(os.path.dirname(__file__), "weights", type, "weight.pth"),
+            map_location=self.device,
+        )
+        self.model = ResNet50(45)  # 폰트 개수에 따라 수정해야함
+        self.model.load_state_dict(pkl.module.state_dict())
+        self.model.to(self.device)
+        self.model.eval()
+
     def classification(self, merged_boxes):
         tmp = []
         for idx, boxes in enumerate(merged_boxes):
             imgs = boxes[3]
-            #dataset = getattr(import_module("dataset_font"), "InfDataset")(imgs, self.resize)
+            # dataset = getattr(import_module("dataset_font"), "InfDataset")(imgs, self.resize)
             dataset = InfDataset(imgs, self.resize)
             loader = torch.utils.data.DataLoader(
                 dataset,
@@ -44,13 +61,15 @@ class Typical_FC:
                 for images in loader:
                     images = images.to(self.device)
                     predict = self.model(images)
-                    pred_topk = torch.topk(predict, k= 5, dim = -1)
+                    pred_topk = torch.topk(predict, k=5, dim=-1)
                     preds = pred_topk.indices
                     values = pred_topk.values
                     for pred, value in zip(preds.cpu().numpy(), values.cpu().numpy()):
-                        for p,v in zip(pred, value):
+                        for p, v in zip(pred, value):
                             preds_dict[p] += v
-            
-            sorted_dict = sorted(preds_dict.items(), key = lambda item: item[0], reverse = True)
+
+            sorted_dict = sorted(
+                preds_dict.items(), key=lambda item: item[0], reverse=True
+            )
             tmp.append(self.font_list[sorted_dict[0][0]])
         return tmp
